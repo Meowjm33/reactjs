0fabd3924150401184a3401e101d2f97b1a7f0a4
surangana thapa <shilpithapa233@gmail.com>
2025-12-28 11:14:55 +0545

    finally original technique is being learned

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import Bmi from "./Bmi";
-import Joke from "./Joke";
-import Login from "./Login";
-import Temperature from "./Temperature";
-
 export default function App(){
   return(
     <>
-  <Login/>
-  <Bmi/>
-  <Temperature/>
-  <Joke/>
+  
     </>
     
   );
