a79f7148d71b4ab1614a231b8eeb1f0e7dd3847c
surangana thapa <shilpithapa233@gmail.com>
2026-01-09 23:10:22 +0545

    added div classname in footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,10 @@
 
 function Footer(){
     return(
-        <>
+        <div className="flex justify-center">
         <p>&copy; {new Date().getFullYear()}2025 Surangana Thapa. All right reserved.</p>
         {/* &copy;  copyright symbol ko lagi use garincha */}
-        </>
+        </div>
     );
 }
 export default Footer;
